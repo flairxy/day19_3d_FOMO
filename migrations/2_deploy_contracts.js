950993b5cc5ef1ea146feaba11469d9b63cd06a1
flairxy <okonkwo.miracle@raadaa.com>
2021-02-06 01:31:50 +0100

    initial(no frontend)

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,5 @@
-const SimpleStorage = artifacts.require("SimpleStorage");
-const TutorialToken = artifacts.require("TutorialToken");
-const ComplexStorage = artifacts.require("ComplexStorage");
+const Fomo3D = artifacts.require("Fomo3D");
 
-module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
-  deployer.deploy(TutorialToken);
-  deployer.deploy(ComplexStorage);
+module.exports = function (deployer) {
+  deployer.deploy(Fomo3D);
 };
